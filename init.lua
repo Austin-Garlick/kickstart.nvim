6a79f922017006773c6b08835fd36b81ca68d7a3
Austin Garlick <aus20gar@gmail.com>
2026-02-09 19:58:41 -0700

    Added diagnostic checking keybinding

--- init.lua
+++ init.lua
@@ -213,6 +213,7 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.keymap.set('n', '<leader>e', vim.cmd.NvimTreeToggle, { desc = 'Toggle file [E]xplorer' })
+vim.keymap.set('n', '<Leader>ds', vim.diagnostic.open_float, { desc = '[D]iagnostic - [S]how' })
 vim.keymap.set('n', '<space>fb', function()
   require('telescope').extensions.file_browser.file_browser()
 end, { desc = 'Open Telescope [F]ile [B]rowser' })
